f9e833d9b6d2ba415d005771ef2f02664a7d768c
iamusayev <musayevanar1337@gmail.com>
2022-07-22 16:06:48 +0400

    added AccountMapper

--- src/main/java/com/example/remondisimpl/mapper/AccountMapper.java
+++ src/main/java/com/example/remondisimpl/mapper/AccountMapper.java
@@ -6,19 +6,25 @@ import com.example.remondisimpl.dto.AccountDto;
 import com.remondis.remap.Mapper;
 import com.remondis.remap.Mapping;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 @Component
 public class AccountMapper {
 
 
     @Bean
-    public Mapper<AccountDto, AccountEntity> mapDtoToEntity(){
+    public Mapper<AccountDto, AccountEntity> mapAccountDtoToAccountEntity(){
         return Mapping.from(AccountDto.class)
                 .to(AccountEntity.class)
                 .omitInDestination(AccountEntity::getId)
+                .mapper();
+    }
+
+    @Bean
+    public Mapper<AccountEntity,AccountDto> mapAccountEntityToAccountDto(){
+        return Mapping.from(AccountEntity.class)
+                .to(AccountDto.class)
+                .omitInSource(AccountEntity::getId)
                 .mapper();
     }
 
