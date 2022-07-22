07751e381aef4c6ff3f03e2c986a8cc49ed0a5f1
iamusayev <musayevanar1337@gmail.com>
2022-07-22 15:25:09 +0400

    added AccountDto

---
+++ src/main/java/com/example/remondisimpl/dto/AccountDto.java
@@ -0,0 +1,14 @@
+package com.example.remondisimpl.dto;
+
+import lombok.*;
+import org.springframework.context.annotation.Lazy;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AccountDto {
+
+    private String number;
+
+}
