228062aea34b222f7805081d81c2a31e71c2c5c1
iamusayev <musayevanar1337@gmail.com>
2022-07-22 15:25:30 +0400

    added UserDto

--- src/main/java/com/example/remondisimpl/dto/UserDto.java
+++ src/main/java/com/example/remondisimpl/dto/UserDto.java
@@ -1,13 +1,24 @@
 package com.example.remondisimpl.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+ import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UserDto {
 
     private String name;
     private String email;
+    private String password;
+    private LocalDate birthday;
+    private String gender;
+    private String role;
+    private String skip;
+    private String change;
+    private AccountDto account;
 
 }
