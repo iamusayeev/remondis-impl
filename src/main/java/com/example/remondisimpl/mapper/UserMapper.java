f94e4c051d9a19334bb8b3a49f0c4db256eb2533
iamusayev <musayevanar1337@gmail.com>
2022-07-22 16:07:15 +0400

    added UserMapper

--- src/main/java/com/example/remondisimpl/mapper/UserMapper.java
+++ src/main/java/com/example/remondisimpl/mapper/UserMapper.java
@@ -19,13 +19,14 @@ public class UserMapper {
 
     private final Mapper<AccountDto, AccountEntity> mapDtoToEntity;
 
+    private final Mapper<AccountEntity,AccountDto> mapAccountEntityToAccountDto;
 
     @Bean
     public Mapper<UserDto, UserEntity> mapDtoToEntityUser() {
         return Mapping.from(UserDto.class)
                 .to(UserEntity.class)
-//                .omitInDestination(UserEntity::getBirthday)
                 .omitInDestination(UserEntity::getId)
+                .omitInDestination(UserEntity::getPassword)
                 .omitInSource(UserDto::getSkip)
                 .replace(UserDto::getRole, UserEntity::getRole)
                 .withSkipWhenNull(Role::valueOf)
@@ -37,12 +38,24 @@ public class UserMapper {
                 .mapper();
     }
 
-//    @Bean
-//    public Mapper<UserEntity, UserDto> mapEntityToDto() {
-//        return Mapping.from(UserEntity.class)
-//                .to(UserDto.class)
-//                .mapper();
-//    }
+
+    @Bean
+    public Mapper<UserEntity , UserDto> mapEntityToDto(){
+        return Mapping.from(UserEntity.class)
+                .to(UserDto.class)
+                .omitInSource(UserEntity::getPassword)
+                .omitInSource(UserEntity::getId)
+                .omitInSource(UserEntity::getSmth)
+                .omitInDestination(UserDto::getChange)
+                .omitInDestination(UserDto::getSkip)
+                .replace(UserEntity::getGender,UserDto::getGender)
+                .withSkipWhenNull(Enum::toString)
+                .replace(UserEntity::getRole,UserDto::getRole)
+                .withSkipWhenNull(Enum::toString)
+                .useMapper(mapAccountEntityToAccountDto)
+                .mapper();
+    }
+
 
 
 }
