31eb5b03e812d8e490294da84959c12f2de609a4
iamusayev <musayevanar1337@gmail.com>
2022-07-22 15:26:08 +0400

    added UserService

--- src/main/java/com/example/remondisimpl/service/UserService.java
+++ src/main/java/com/example/remondisimpl/service/UserService.java
@@ -3,7 +3,7 @@ package com.example.remondisimpl.service;
 import com.example.remondisimpl.dao.entity.UserEntity;
 import com.example.remondisimpl.dao.repository.UserRepository;
 import com.example.remondisimpl.dto.UserDto;
-import com.example.remondisimpl.mapper.UserMapper;
+//import com.example.remondisimpl.mapper.UserMapper;
 import com.remondis.remap.Mapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,23 +13,21 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
-    private final UserMapper userMapper;
+    private final Mapper<UserDto,UserEntity> mapDtoToEntity;
 
 
     public void save(UserDto userDto) {
-        Mapper<UserDto, UserEntity> userDtoUserEntityMapper = userMapper.mapDtoToEntity();
-        UserEntity userEntity = userDtoUserEntityMapper.map(userDto);
+        UserEntity userEntity = mapDtoToEntity.map(userDto);
         userRepository.save(userEntity);
     }
 
 
-    public UserDto findById(Integer id) {
-        UserEntity userEntity = fetchUserIfExist(id);
-        Mapper<UserEntity, UserDto> userDtoUserEntityMapper = userMapper.mapEntityToDto();
-        UserDto userDto = userDtoUserEntityMapper.map(userEntity);
-
-        return userDto;
-    }
+//    public UserDto findById(Integer id) {
+//        UserEntity userEntity = fetchUserIfExist(id);
+//
+//
+//        return userDto;
+//    }
 
     private UserEntity fetchUserIfExist(Integer id) {
         return userRepository.findById(id).orElseThrow(RuntimeException::new);
